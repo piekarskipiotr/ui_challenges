8b3d591022666aa104b1b3ebbd924e79274c770b
piekarskipiotr <ppiekarski8@gmail.com>
2023-07-05 20:34:56 +0200

    create app bar

---
+++ lib/challenges/tiktok/view/rectangle_tab_indicator.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RectangleTabIndicator extends Decoration {
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _RectangleTabIndicatorPainter(this, onChanged);
+  }
+}
+
+class _RectangleTabIndicatorPainter extends BoxPainter {
+  _RectangleTabIndicatorPainter(this.decoration, VoidCallback? onChanged)
+      : super(onChanged);
+  final RectangleTabIndicator decoration;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final rect = offset & configuration.size!;
+    final paint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.fill;
+
+    const indicatorHeight = 4.0;
+    final indicatorWidth = rect.width / 2;
+    final dx = rect.left + (rect.width - indicatorWidth) / 2;
+    final dy = rect.bottom - indicatorHeight;
+
+    final indicatorRect = Rect.fromLTWH(
+      dx,
+      dy,
+      indicatorWidth,
+      indicatorHeight,
+    );
+
+    canvas.drawRect(indicatorRect, paint);
+  }
+}
--- lib/challenges/tiktok/view/tiktok_page.dart
+++ lib/challenges/tiktok/view/tiktok_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_challenges/challenges/tiktok/view/tiktok_app_bar.dart';
 
 class TikTokPage extends StatefulWidget {
   const TikTokPage({super.key});
@@ -10,9 +11,16 @@ class TikTokPage extends StatefulWidget {
 class _TikTokPageState extends State<TikTokPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TikTok'),
+    return const DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: TikTokAppBar(),
+        body: TabBarView(
+          children: [
+            Center(child: Text('Following page')),
+            Center(child: Text('For you page')),
+          ],
+        ),
       ),
     );
   }
